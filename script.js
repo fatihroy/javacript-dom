41ccd294dc3b62561ce8cdb909507e33e8bedc5e
fatihroy <afroyhan1@gmail.com>
2022-01-15 18:15:45 +0700

    DOM Manipulation

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-// // DOM Selection 2
+// DOM Manipulation
 
-// // 1. document.querySelector() --> element
-// const p4 = document.querySelector('section#b p');
-//     // merubah warna teks
-//     p4.style.color = 'green'
-//     // memperbesar teks
-//     p4.style.fontSize = '40px'
+// 1. innerHTML
+// const judul = document.getElementById('judul')
+// judul.innerHTML = '<em>My Name Is Abdillah<em>'
 
-// const li3 = document.querySelector('section#b ul li:nth-child(3)');
-//     li3.style.backgroundColor = 'lightskyblue'
+// const sectionA = document.querySelector('section#a')
+// sectionA.innerHTML = '<h2>Hai Abdillah<h2>'
 
+// 2. element.style.<propertyCSS>
+// const judul = document.querySelector('h1#judul')
+// judul.style.color = 'blue'
 
+// 3. element.setAttribute()
+const judul = document.getElementsByTagName('h1')[0]
+judul.setAttribute('name', 'fatih') //name = fatih
 
-// //  2. document.querySelectorAll()
-// const li = document.querySelectorAll('li')
-// li[0].style.color = 'green'
-// li[1].style.color = 'green'
+// 4. element.getAttribute()
+// console.log(judul.getAttribute('id'))
 
+// 5. element.removeAttribute()
+// const ig = document.querySelector('section#a a')
+// ig.removeAttribute('href')
 
-const sectionB = document.getElementById('b')
-// pake sectionB
-const p4 = sectionB.querySelector('p')
-    p4.style.color = 'green'
-    p4.style.fontSize = '40px'
+// 6. element.classList.add()
+// const p3 = document.querySelectorAll('section#a .p3')[0]
+// p3.classList.add('label')
+
+// 7. element.classList.remove()
+// const p3 = document.querySelectorAll('section#a .p3')[0]
+// p3.classList.remove('.p3')
+
+// 8. element.classList.toggle() --> kalau ada dihapus, kalau tidak ada ditambahkan
+// const p3 = document.querySelectorAll('section#a .p3')[0]
+// p3.classList.toggle('label')
+
+// 9. element.classList.item()
+// const p3 = document.querySelectorAll('section#a .p3')[0]
+// p3.classList.add('label')
+// console.log(p3.classList.item(1))
+
+// 10. element.classList.contains()
+// const p3 = document.querySelectorAll('section#a .p3')[0]
+// p3.classList.add('label')
+// console.log(p3.classList.contains('label'))
+
+// 11. element.classList.replace()
+const p3 = document.querySelectorAll('section#a .p3')[0]
+p3.classList.add('label')
+p3.classList.replace('label', 'label1')
+console.log(p3.classList.contains('label'))
