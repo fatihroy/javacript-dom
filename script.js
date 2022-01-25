9e07225d94ab7bd294bad8150938a3380397a49e
fatihroy <afroyhan1@gmail.com>
2022-01-25 17:45:02 +0700

    Dom Traversal

--- script.js
+++ script.js
@@ -1,127 +1,39 @@
-function pilihan_komputer() {
-      // menangkap pilihan computer
-    // membangkitkan bilangan random
-    var comp = Math.random();
+// dom traversal itu adalah penelusuran
 
-    if( comp < 0.34 ) return 'gajah';
-    if( comp >= 0.34 && comp < 0.67 ) return'orang';
-    return 'semut';
-    
-}
-// bikin fungsi spy gambar komputernya berputar dulu
-
-     function putar() {
-          imgCom = document.querySelector('.img-komputer')
-          gambar = ['gajah', 'semut', 'orang']
-          let i = 0
-          const waktuAwal = new Date().getTime()
-     setInterval(function()  {
-          if (new Date().getTime() - waktuAwal > 1000) {
-               clearInterval
-               return 0;
-          }
-          imgCom.setAttribute('src', 'img/' + gambar[i++] +'.png')
-          if (i == gambar.length) {
-               i = 0
-          }
-     }, 100);
-          }
-     
- 
-
-
-
-function hasil(com, player) {
-     // menentukan rules
-     
-     if( player == com) return  'SERI!';
-     if( player == 'gajah' )    return ( com == 'orang' ) ? 'MENANG!' : 'KALAH!';
-     if( player == 'orang' )  return ( com == 'gajah' ) ? 'KALAH!' : 'MENANG!';
-     if( player == 'semut' ) return ( com == 'orang' ) ? 'KALAH' : 'MENANG!';
-   
- 
-}
-  
-
-// cara efektif
-               const imgPilihan = document.querySelectorAll('li img')
-               imgPilihan.forEach(function (gambar) { //parameternya adalah gambar yang ada di li
-
-
-          gambar.addEventListener('click', function () {
-                    const player = gambar
-
-                    const pilihanKomputer = pilihan_komputer()
-                    const pilihanPlayer = player.className // mengambil nama kelas yaitu misal: gajah
-                    hasil = hasil(pilihanKomputer, pilihanPlayer)
-
-           // jalankan fungsi putar
-                    putar()
-                
-                    setTimeout(function () {
-                         // ganti gambar pilihan komputer
-                            const imgcom = document.querySelector('.img-komputer')
-                            imgcom.setAttribute('src', 'img/'+ pilihanKomputer + '.png')
-             
-                            // tampilkan hasil
-                            const info = document.querySelector('.info')
-                            info.innerHTML = hasil
-                                 }, 1000)
-              
-
-
-
-})})
-
-
-
-// cara yang tydak efektif
-// // menangkap pilihan semut
-// const Psemut = document.querySelector('.semut')
-// Psemut.addEventListener('click', function () {
-//      const pilihanKomputer = pilihan_komputer()
-//      const pilihanPlayer = Psemut.className// mengambil nama kelas yaitu gajah
-// hasil = hasil(pilihanKomputer, pilihanPlayer)
-     
-// // ganti gambar pilihan komputer
-// const imgcom = document.querySelector('.img-komputer')
-// imgcom.setAttribute('src', 'img/'+ pilihanKomputer + '.png')
-
-// // tampilkan hasil
-// const info = document.querySelector('.info')
-// info.innerHTML = hasil
+// // bikin program supaya tombol x ketika diklik akan hilang
+// const close = document.querySelector('.close');
+// const card = document.querySelector('.card');
+// // hanya bekerja pada satu elemen (afroyhan aja) kalo pake queryselector/penyeleksi satu elemen
+// close.addEventListener('click', function () {
+//     card.style.display = 'none';
 // })
 
+const close = document.querySelectorAll('.close')
+// // penelusuran
+// for (let index = 0; index < close.length; index++) {
+//     // e berisi event yang sedang terjadi
+//     close[index].addEventListener('click', function (e) {
+//     // // parentElement = adalah dom traversalnya
+//     //     close[index].parentElement.style.display = 'none' //ambil parentnya yaitu card dan ganti displaynya jadi none
+// // console.log(e) //untuk menamplan berbagai informasi e
+// e.target.parentElement.style.display = 'none' //cara ini sama kayak yang diatas
+//     })
+// }
+
+//e adalah untuk ambil satu elemen
+close.forEach(function (e) {
+    e.addEventListener('click', function (e) {
+        e.target.parentElement.style.display = 'none'
+    })
+})
+
+// mengembalikan :                  node         element       node          element                  node               element
+// method-method dom traversal --> parentNode, parentElement, nextSibling, nextElementSibling, previousSibling, previousElementSibling
 
 
-// // menangkap pilihan orang
-// const Porang = document.querySelector('.orang')
-// Porang.addEventListener('click', function () {
-//      const pilihanKomputer = pilihan_komputer()
-//      const pilihanPlayer = Porang.className// mengambil nama kelas yaitu gajah
-// hasil = hasil(pilihanKomputer, pilihanPlayer)
-     
-// // ganti gambar pilihan komputer
-// const imgcom = document.querySelector('.img-komputer')
-// imgcom.setAttribute('src', 'img/'+ pilihanKomputer + '.png')
-
-// // tampilkan hasil
-// const info = document.querySelector('.info')
-// info.innerHTML = hasil
-// })
-
-// // menangkap pilihan gajah
-// const Pgajah = document.querySelector('.gajah')
-// Pgajah.addEventListener('click', function () {
-//      const pilihanKomputer = pilihan_komputer()
-//      const pilihanPlayer = Pgajah.className// mengambil nama kelas yaitu gajah
-// hasil = hasil(pilihanKomputer, pilihanPlayer)
-     
-// // ganti gambar pilihan komputer
-// const imgcom = document.querySelector('.img-komputer')
-// imgcom.setAttribute('src', 'img/'+ pilihanKomputer + '.png')
-
-// // tampilkan hasil
-// const info = document.querySelector('.info')
-// info.innerHTML = hasil
-// })
+// mencoba
+// kalau element itu mengabaikan enter/spasi
+const nama = document.querySelector('.nama')
+console.log(nama.parentElement.parentElement); //ambil kakek
+// kalau node gak boleh pake enter dan spasi misal:
+console.log(nama.nextSibling);
