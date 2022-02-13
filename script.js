0796fe516187b48f1d5d24d6b795a2b16f86a32d
fatihroy <afroyhan1@gmail.com>
2022-02-13 10:38:24 +0700

    latihan bikin gallery

--- script.js
+++ script.js
@@ -1,53 +1,23 @@
-// dom traversal itu adalah penelusuran
-
-// // bikin program supaya tombol x ketika diklik akan hilang
-// const close = document.querySelector('.close');
-// const card = document.querySelector('.card');
-// // hanya bekerja pada satu elemen (afroyhan aja) kalo pake queryselector/penyeleksi satu elemen
-// close.addEventListener('click', function () {
-//     card.style.display = 'none';
-// })
-
-const close = document.querySelectorAll('.close')
-// // penelusuran
-// for (let index = 0; index < close.length; index++) {
-//     // e berisi event yang sedang terjadi
-//     close[index].addEventListener('click', function (e) {
-//     // // parentElement = adalah dom traversalnya
-//     //     close[index].parentElement.style.display = 'none' //ambil parentnya yaitu card dan ganti displaynya jadi none
-// // console.log(e) //untuk menamplan berbagai informasi e
-// e.target.parentElement.style.display = 'none' //cara ini sama kayak yang diatas
-//     })
-// }
-
-//e adalah untuk ambil satu elemen
-// close.forEach(function (e) {
-//     e.addEventListener('click', function (e) {
-//         e.target.parentElement.style.display = 'none'
-//         // prevent default --> untuk menghentikan semua aksi default yang dilakukan oleh sebuah elemen
-//         e.preventDefault(); //defaultnya elemen a pd html merefresh halaman
-//         e.stopPropagation() //--> spy tidak melakukan event bubling
-//     })
-// })
-
-// // event bubling --> ktk kita punya event pd suatu elemen, dan  elemen pembungkusnya juga ada even maka even elemen pembungkus akan dijalankan terlebih dahulu
-// const cards = document.querySelectorAll('.card')
-// cards.forEach(function(c) {
-//     c.addEventListener('click', function () {
-//         alert('allright')
-//     })
-// })
-  
-
-
-
-// pake event bubling spy tobol close lebih efektif
 const container = document.querySelector('.container')
-// e adalah element pada container
-container.addEventListener('click', function(e) {
-//jika yang diklik memiliki class close maka:
- if (e.target.className == 'close') {
-     e.target.parentElement.style.display = 'none'
-e.preventDefault()
- }
-})                            
+const big = document.querySelector('.big')
+const thumbs = document.querySelectorAll('.thumb')
+container.addEventListener('click', function (e) {
+    // cek apakah yang diklik adalah thumb
+if (e.target.className == 'thumb') {
+    big.src = e.target.src // src jumbo akan diganti dengan srcnya thumb
+    big.classList.add('fade') //big dikasih class baru yaitu fade jadinya hilang karena animasinya kembali keawal 
+    setTimeout(function(){
+        big.classList.remove('fade')
+    }, 500);
+    thumbs.forEach(function(thumb){
+        // if(thumb.classList.contains('active')){
+        //     thumb.classList.remove('active')
+        // }
+      
+        thumb.className = 'thumb'
+       
+    })
+    e.target.classList.add('active')
+    
+}
+})
